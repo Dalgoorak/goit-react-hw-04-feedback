0377ff0421709002dbdcb9de6402aab7aa54dca8
Dalgoorak <103432002+Dalgoorak@users.noreply.github.com>
2022-10-18 11:14:41 +0300

    add PropTypes

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,4 +1,5 @@
 import css from './FeedbackOptions.module.css';
+import PropTypes from 'prop-types';
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
@@ -18,31 +19,7 @@ export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   );
 };
 
-// import PropTypes from 'prop-types';
-// import css from './FeedbackOptions.module.css';
-
-// function FeedbackOptions({ options, onLeaveFeedback }) {
-//   return (
-//     <ul className={css.feedback__options}>
-//       {options.map(option => (
-//         <li className={css.item} key={option}>
-//           <button
-//             className={css.button}
-//             type="button"
-//             name={option}
-//             onClick={onLeaveFeedback}
-//           >
-//             {option}
-//           </button>
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// }
-
-// FeedbackOptions.prototype = {
-//   options: PropTypes.arrayOf(PropTypes.string).isRequired,
-//   onLeaveFeedback: PropTypes.func.isRequired,
-// };
-
-// export default FeedbackOptions;
+FeedbackOptions.PropType = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
